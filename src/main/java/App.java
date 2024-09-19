1b0a253a26808c8e43244d98e7876b5fc23bfcf2
Felix <93457386+feliquisds@users.noreply.github.com>
2024-09-19 00:07:36 -0300

    feat(main); added main code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,13 @@
 package main.java;
-import java.util.Scanner;
 
 public class App {
-        //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
     public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
+        CorpoHumano c1 = new CorpoHumano(0, 0, 0, 0);
+        c1.setMassa(65);
+        c1.setVolume(0.07);
+        c1.setDensidade(2000);
+        c1.setAltura(1.7);
+        System.out.println(c1.calcularIMC());
     }
 }
